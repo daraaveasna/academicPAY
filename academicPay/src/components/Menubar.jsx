8d5d499a0233e587b9bbf9848f8826fa4e8621a4
POM-MOUYLANG <126035070+POMMOUYLANG@users.noreply.github.com>
2023-09-12 16:05:36 +0700

    update

---
+++ academicPay/src/components/Menubar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import "../components/Menubar.css"
+import {FiChevronUp,FiSearch} from "react-icons/fi"
+import logoitc from "../assets/ITC.png"
+import Dropdet from './Dropdet'
+import Dropyear from "./Dropyear"
+
+const Menubar = () => {
+  return (
+    <div className='menubar'>
+        <div className='left'>
+            <img src={logoitc} alt="" />
+            <div className='double-left'>
+              <div className='dropdown dropdown-year'>
+                {/* <button>
+                  <span>2023</span>
+                  <FaChevronUp/>
+                </button> */}
+                <Dropyear/>
+              </div>
+              <div className='dropdown-det'>
+                {/* <button>
+                  <span>ALL</span>
+                  <FaChevronUp/>
+                </button> */}
+                <Dropdet/>
+              </div>
+            </div>
+        </div>
+        <div className='right'>
+            <div className="search-bar">
+              <FiSearch className='search'/>
+              <form action="">
+                <input type="text" placeholder='search' />
+              </form>
+            </div>
+            <button className='btn-add'><h1>+</h1></button>
+        </div>
+    </div>
+  )
+}
+
+export default Menubar
